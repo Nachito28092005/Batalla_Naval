f5cb60eb3f70f7dcf1c7096ef8c93d6573bc6041
Nachito28092005 <131969080+Nachito28092005@users.noreply.github.com>
2023-06-21 08:15:53 -0300

    Update Prueba Batalla Naval.cpp

--- Prueba Batalla Naval.cpp
+++ Prueba Batalla Naval.cpp
@@ -25,7 +25,7 @@ void imprimirGrafico(char tablero[FILAS][COLUMNAS]) {
     printf("\nGrafico de barcos:\n");
     for (int i = 0; i < FILAS; i++) {
         for (int j = 0; j < COLUMNAS; j++) {
-            if (tablero[i][j] == static_cast<char>(219)) {
+            if (tablero[i][j] == 'B') {
                 printf("|O");
             } else {
                 printf("| ");
@@ -76,7 +76,7 @@ void colocarBarcosUsuario(char tablero[FILAS][COLUMNAS]) {
         scanf(" %c", &orientacion);
 
         if (fila < 0 || fila >= FILAS || columna < 0 || columna >= COLUMNAS) {
-            printf("Coordenadas no válidas. Vuelva a intentarlo.\n");
+            printf("Coordenadas no vÃ¡lidas. Vuelva a intentarlo.\n");
             i--;
             continue;
         }
@@ -87,7 +87,7 @@ void colocarBarcosUsuario(char tablero[FILAS][COLUMNAS]) {
                 valido = 0;
             } else {
                 for (int j = 0; j < tamano; j++) {
-                    if (tablero[fila + j][columna] == static_cast<char>(219)) {
+                    if (tablero[fila + j][columna] == 'B') {
                         valido = 0;
                         break;
                     }
@@ -98,7 +98,7 @@ void colocarBarcosUsuario(char tablero[FILAS][COLUMNAS]) {
                 valido = 0;
             } else {
                 for (int j = 0; j < tamano; j++) {
-                    if (tablero[fila][columna + j] == static_cast<char>(254)) {
+                    if (tablero[fila][columna + j] == 'B') {
                         valido = 0;
                         break;
                     }
@@ -109,18 +109,18 @@ void colocarBarcosUsuario(char tablero[FILAS][COLUMNAS]) {
         }
 
         if (!valido) {
-            printf("Posición no válida. Vuelva a intentarlo.\n");
+            printf("PosiciÃ³n no vÃ¡lida. Vuelva a intentarlo.\n");
             i--;
             continue;
         }
 
         if (orientacion == 'V' || orientacion == 'v') {
             for (int j = 0; j < tamano; j++) {
-                tablero[fila + j][columna] = static_cast<char>(219);
+                tablero[fila + j][columna] = 'B';
             }
         } else if (orientacion == 'H' || orientacion == 'h') {
             for (int j = 0; j < tamano; j++) {
-                tablero[fila][columna + j] = static_cast<char>(254);
+                tablero[fila][columna + j] = 'B';
             }
         }
 
@@ -152,7 +152,7 @@ void colocarBarcosMaquina(char tablero[FILAS][COLUMNAS]) {
                 valido = 0;
             } else {
                 for (int j = 0; j < tamano; j++) {
-                    if (tablero[fila + j][columna] == static_cast<char>(219)) {
+                    if (tablero[fila + j][columna] == 'B') {
                         valido = 0;
                         break;
                     }
@@ -163,7 +163,7 @@ void colocarBarcosMaquina(char tablero[FILAS][COLUMNAS]) {
                 valido = 0;
             } else {
                 for (int j = 0; j < tamano; j++) {
-                    if (tablero[fila][columna + j] == static_cast<char>(219)) {
+                    if (tablero[fila][columna + j] == 'B') {
                         valido = 0;
                         break;
                     }
@@ -178,31 +178,13 @@ void colocarBarcosMaquina(char tablero[FILAS][COLUMNAS]) {
 
         if (orientacion == 0) {
             for (int j = 0; j < tamano; j++) {
-                tablero[fila + j][columna] = static_cast<char>(219);
+                tablero[fila + j][columna] = 'B';
             }
         } else if (orientacion == 1) {
             for (int j = 0; j < tamano; j++) {
-                tablero[fila][columna + j] = static_cast<char>(219);
+                tablero[fila][columna + j] = 'B';
             }
         }
-    }
-}
-
-int realizarDisparo(char tablero[FILAS][COLUMNAS], char tableroDisparos[FILAS][COLUMNAS], int fila, int columna) {
-    if (fila < 0 || fila >= FILAS || columna < 0 || columna >= COLUMNAS) {
-        return 0; // Disparo fuera de los límites del tablero
-    }
-
-    if (tableroDisparos[fila][columna] != ' ') {
-        return 0; // Ya se ha disparado en esta posición anteriormente
-    }
-
-    if (tablero[fila][columna] == static_cast<char>(219)) {
-        tableroDisparos[fila][columna] = 'X'; // Disparo acertado
-        return 1;
-    } else {
-        tableroDisparos[fila][columna] = 'O'; // Disparo fallido
-        return -1;
     }
 }
 
@@ -237,60 +219,7 @@ int main() {
     printf("\nTablero de disparos del usuario:\n");
     imprimirTableroDisparos(tableroDisparosUsuario);
 
-    // Comienza el juego
-    int barcosUsuario = 6;
-    int barcosMaquina = 6;
-    int turno = 0; // 0 para el usuario, 1 para la máquina
-
-    while (barcosUsuario > 0 && barcosMaquina > 0) {
-        int fila, columna;
-
-        if (turno == 0) {
-            printf("\nTurno del usuario:\n");
-            printf("Ingrese la fila para el disparo: ");
-            scanf("%d", &fila);
-
-            printf("Ingrese la columna para el disparo: ");
-            scanf("%d", &columna);
-
-            int resultado = realizarDisparo(tableroMaquina, tableroDisparosUsuario, fila, columna);
-            if (resultado == 1) {
-                printf("¡Has acertado un barco!\n");
-                barcosMaquina--;
-            } else if (resultado == -1) {
-                printf("Has disparado al agua.\n");
-            } else {
-                printf("Disparo inválido. Vuelve a intentarlo.\n");
-                continue;
-            }
-
-            printf("\nTablero de disparos del usuario:\n");
-            imprimirTableroDisparos(tableroDisparosUsuario);
-        } else {
-            printf("\nTurno de la máquina:\n");
-            fila = rand() % FILAS;
-            columna = rand() % COLUMNAS;
-
-            int resultado = realizarDisparo(tableroUsuario, tableroDisparosMaquina, fila, columna);
-            if (resultado == 1) {
-                printf("La máquina ha acertado un barco.\n");
-                barcosUsuario--;
-            } else if (resultado == -1) {
-                printf("La máquina ha disparado al agua.\n");
-            }
-
-            printf("\nTablero de disparos de la máquina:\n");
-            imprimirTableroDisparos(tableroDisparosMaquina);
-        }
-
-        turno = (turno + 1) % 2;
-    }
-
-    if (barcosUsuario == 0) {
-        printf("¡Has ganado! Has hundido todos los barcos de la máquina.\n");
-    } else {
-        printf("¡Has perdido! La máquina ha hundido todos tus barcos.\n");
-    }
+    // Comienza el juego...
 
     return 0;
 }
